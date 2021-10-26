2770a8b27c47668b4761379c72e3d4d388e45cb2
Maxwel <maxwelsant96@icloud.com>
2021-10-26 15:33:19 -0300

    Initialize project spring

---
+++ src/main/java/io/github/maxwelsantana/travelsjavaapi/TravelsJavaApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.maxwelsantana.travelsjavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelsJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelsJavaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/maxwelsantana/travelsjavaapi/TravelsJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.maxwelsantana.travelsjavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelsJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
